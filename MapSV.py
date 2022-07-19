36216b051c64270c3149cefc52840f1caf4cb10e
Protochuu <chat.et.coq@gmail.com>
2022-07-19 15:13:10 -0400

    implementacion Insert de MapSV

--- MapSV.py
+++ MapSV.py
@@ -1,34 +1,30 @@
+from Map import Map
 
-class Node(object):
+class MapSV(Map):
     def __init__(self):
-        self.root = []
-        self.children = [None]
-
-    def insert(self,pairLeaf):
-        self.root.append(pairLeaf)
-        self.root.sort()
-        print(self.children)
-
-        if len(self.root) == 5:
-            print("se entro en el if")
-            leafLeft = self.root[0:2]
-            leafRight = self.root[3:]
-            leafCenter = self.root[2]
-            print((leafCenter))
-            print(leafLeft)
-            print(leafRight)
+        self.root = None
+        self.innerArray = []
+    def binaryInsert(self, key: str, value: int):
+        center = len(self.innerArray) // 2
+        centerValue = self.innerArray[center]
+        leftIndex = 0
+        rightIndex = len(self.innerArray)
+        while True:
+            center = (leftIndex + rightIndex) // 2
+            if centerValue[0] == key:
+                raise KeyError
+            elif centerValue[0] < key:
+                leftIndex = center + 1
+                if rightIndex - leftIndex == 1:
+                    self.innerArray.insert(rightIndex, (key, value))
+                    break
+            elif centerValue[0] > key:
+                rightIndex = center
+                if rightIndex - leftIndex == 1:
+                    self.innerArray.insert(leftIndex, (key, value))
+                    break
+    def insert(self, key: str, value: int):
+        if len(self.innerArray) == 0:
+            self.innerArray.append((key,value))
         else:
-            self.children.append(None)
-
-
-
-gatos = Node()
-gatos.insert(("popo",1))
-gatos.insert(("popo",4))
-gatos.insert(("popo",2))
-gatos.insert(("popo",6))
-gatos.insert(("popo",5))
-
-perros = Node()
-gatos.children[0] = perros
-print(gatos.children)
+            self.binaryInsert(key,value)
