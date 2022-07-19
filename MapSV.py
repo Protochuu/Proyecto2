e50bb74b8067721385ab6a12cd652b32391b31c5
Protochuu <chat.et.coq@gmail.com>
2022-07-19 15:41:21 -0400

    se agregaron metodos erase y at(incompleto)

--- MapSV.py
+++ MapSV.py
@@ -23,8 +23,20 @@ class MapSV(Map):
                 if rightIndex - leftIndex == 1:
                     self.innerArray.insert(leftIndex, (key, value))
                     break
+    def binarySearch:
+
     def insert(self, key: str, value: int):
         if len(self.innerArray) == 0:
             self.innerArray.append((key,value))
         else:
             self.binaryInsert(key,value)
+    def erase(self, key: str):
+        if len(self.innerArray) == 0:
+            raise IndexError
+        for i in range(len(self.innerArray)):
+            element = self.innerArray[i]
+            if element[0] == key:
+                self.innerArray.remove(element)
+                return
+        raise KeyError
+    def at(self, key: str) -> int:
